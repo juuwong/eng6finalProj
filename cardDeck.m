b2cdaa81720ee4b9f61394df2c5e6b7b6c57cc7e
May Eusebio <mariellejde@gmail.com>
2019-11-26 11:44:51 -0800

    52 cards in deck

--- cardDeck.m
+++ cardDeck.m
@@ -1,8 +1,11 @@
 function cardDeck(app)
-if(testA < 3)
+
+if(app.testA < 3)
 app.img = "cardPhotos/2_of_clubs.png";
-elseif(testA > 3)
+elseif(app.testA > 3)
 app.img = "cardPhotos/2_of_diamonds.png";
 end
-app.Image.ImageSource = app.img;  
+app.Image.ImageSource = app.img;
+
+save cardDeck.mat 
 end
