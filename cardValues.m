9ff3c24c2579fb30e7955d2cae9ca325ef3c1fdd
May Eusebio <mariellejde@gmail.com>
2019-11-27 11:12:25 -0800

    File manip

--- cardValues.m
+++ cardValues.m
@@ -1,15 +1,11 @@
-    imagefiles = dir('*.png');      
-    nfiles = length(imagefiles); 
-    for ii=1:nfiles
-       currentfilename = imagefiles(ii).name;
-       imgFileNames{ii} = char(currentfilename);
-    end
-     
-    cardId = [1:52];
-    v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
-    cardValue = repelem(v,4);
-    cards = [cardId', cardValue', string(imgFileNames)'];
-    
-    
-    
-    
+imagefiles = dir('cardPhotos/*.png');      
+nfiles = length(imagefiles); 
+for ii=1:nfiles
+   currentfilename = imagefiles(ii).name;
+   imgFileNames{ii} = char(currentfilename);
+end
+       
+cardId = [1:52];
+ v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
+cardValue = repelem(v,4);
+cards = [cardId', cardValue', string(imgFileNames)'];
--- getCards.m
+++ getCards.m
@@ -1,15 +1,18 @@
 function getCards(app)
-    imagefiles = dir('*.png');      
-    nfiles = length(imagefiles); 
-    for ii=1:nfiles
-       currentfilename = imagefiles(ii).name;
-       app.imgFileNames{ii} = char(currentfilename);
-    end
+imagefiles = dir('cardPhotos/*.png');      
+nfiles = length(imagefiles); 
+for ii=1:nfiles
+   currentfilename = imagefiles(ii).name;
+   imgFileNames{ii} = char(currentfilename);
+end
     
-    %assigning values to each of the cards according to their card photo
-    v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
-    app.cardValues = repelem(v,4);
-    [row, col] = size(app.imgFileNames);
+%     %assigning values to each of the cards according to their card photo
+%     v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
+%     app.cardValues = repelem(v,4);
+%     [row, col] = size(app.imgFileNames);
+%     
+%     suits = ['clubs' 'hearts' 'diamonds' 'spades'];
+%     repelem(suits,13)
     
     % app.Image.ImageSource = app.imgFileNames{1};
 end
