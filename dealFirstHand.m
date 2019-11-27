47712d183ea71e392bd1f74502ef5e5f7ee061a8
May Eusebio <mariellejde@gmail.com>
2019-11-27 11:41:16 -0800

    Created cards 2d array

--- dealFirstHand.m
+++ dealFirstHand.m
@@ -3,19 +3,19 @@ function dealFirstHand(app)
 if(app.numPlayers == 1)
     % Dealer Hand
     app.Image_3.ImageSource = "faceDownCard.png";
-    app.Image2_3.ImageSource = app.imgFileNames{1};
+    app.Image2_3.ImageSource = app.cards{1,3};
     %Player 1
-    app.Image.ImageSource = app.imgFileNames{2};
-    app.Image2.ImageSource = app.imgFileNames{3};
+    app.Image.ImageSource = app.cards{2,3};
+    app.Image2.ImageSource = app.cards{3,3};
     elseif(app.numPlayers == 2)
     % Dealer Hand
     app.Image_3.ImageSource = "faceDownCard.png";
-    app.Image2_3.ImageSource = app.imgFileNames{1};
+    app.Image2_3.ImageSource = app.cards{1,3};
     %Player 1
-    app.Image.ImageSource = app.imgFileNames{2};
-    app.Image2.ImageSource = app.imgFileNames{3};
+    app.Image.ImageSource = app.cards{2,3};
+    app.Image2.ImageSource = app.cards{3,3};
     %Player 2
-    app.Image_2.ImageSource = app.imgFileNames{4};
-    app.Image2_2.ImageSource = app.imgFileNames{5};
+    app.Image_2.ImageSource = app.cards{4,3};
+    app.Image2_2.ImageSource = app.cards{5,3};
 end
 end
--- getCards.m
+++ getCards.m
@@ -3,16 +3,12 @@ imagefiles = dir('cardPhotos/*.png');
 nfiles = length(imagefiles); 
 for ii=1:nfiles
    currentfilename = imagefiles(ii).name;
-   imgFileNames{ii} = char(currentfilename);
+   app.imgFileNames{ii} = char(currentfilename);
 end
     
-%     %assigning values to each of the cards according to their card photo
-%     v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
-%     app.cardValues = repelem(v,4);
-%     [row, col] = size(app.imgFileNames);
-%     
-%     suits = ['clubs' 'hearts' 'diamonds' 'spades'];
-%     repelem(suits,13)
-    
-    % app.Image.ImageSource = app.imgFileNames{1};
+cardId = [1:52];
+v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
+cardValue = repelem(v,4);
+app.cards = [cardId', cardValue', "cardPhotos/" + string(app.imgFileNames)'];
+
 end
