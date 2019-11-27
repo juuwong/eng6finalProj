48b26a8375476a37583fb8f42e7a7305297286be
Justin Wong <juuwong@ucdavis.edu>
2019-11-27 12:26:20 -0800

    Added Reset and Quit Button Functionality to startScreen. Updated getCards.m

--- getCards.m
+++ getCards.m
@@ -1,18 +1,19 @@
-function getCards(app)
+function getCards(app,i)
 imagefiles = dir('cardPhotos/*.png');      
 nfiles = length(imagefiles); 
 for ii=1:nfiles
    currentfilename = imagefiles(ii).name;
    imgFileNames{ii} = char(currentfilename);
 end
-    
-%     %assigning values to each of the cards according to their card photo
-%     v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
-%     app.cardValues = repelem(v,4);
-%     [row, col] = size(app.imgFileNames);
-%     
-%     suits = ['clubs' 'hearts' 'diamonds' 'spades'];
-%     repelem(suits,13)
-    
-    % app.Image.ImageSource = app.imgFileNames{1};
+       
+cardId = [1:52];
+ v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
+cardValue = repelem(v,4);
+cards = [cardId', cardValue', 'cardPhotos/' + string(imgFileNames)'];
+
+cardCall = randperm(52)
+
+takeCard = cards(cardCall(i),:)
+
+
 end
