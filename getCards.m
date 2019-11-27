0266494b60c157c3bbd0949cc7f335810dd251ec
May Eusebio <mariellejde@gmail.com>
2019-11-26 16:03:14 -0800

    Trying to assign values

--- getCards.m
+++ getCards.m
@@ -6,5 +6,10 @@ function getCards(app)
        app.imgFileNames{ii} = char(currentfilename);
     end
     
+    %assigning values to each of the cards according to their card photo
+    v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
+    app.cardValues = repelem(v,4);
+    [row, col] = size(app.imgFileNames);
+    
     % app.Image.ImageSource = app.imgFileNames{1};
 end
