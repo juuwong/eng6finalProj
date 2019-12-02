147b7ff2acff7e824fa8c9791665a2f5c33e6041
Justin Wong <juuwong@ucdavis.edu>
2019-12-02 14:19:24 -0800

    cardDeck and cardValues already implemented in dealFirstHand and getCards. Moved them to unused folder.

--- getCards.m
+++ getCards.m
@@ -5,23 +5,10 @@ for ii=1:nfiles
    currentfilename = imagefiles(ii).name;
    app.imgFileNames{ii} = char(currentfilename);
 end
-<<<<<<< HEAD
-       
-cardId = [1:52];
- v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
-cardValue = repelem(v,4);
-cards = [cardId', cardValue', 'cardPhotos/' + string(imgFileNames)'];
-
-cardCall = randperm(52)
-
-takeCard = cards(cardCall(i),:)
-
-=======
     
 cardId = [1:52];
 v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
 cardValue = repelem(v,4);
 app.cards = [cardId', cardValue', "cardPhotos/" + string(app.imgFileNames)'];
->>>>>>> master
 
 end
