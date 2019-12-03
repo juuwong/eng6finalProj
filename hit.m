956da416634840b239a967a914c39345373bc812
zachRama <57784811+zachRama@users.noreply.github.com>
2019-12-03 14:13:15 -0800

    I implemented the stand fcn
    and it is wonderful

--- hit.m
+++ hit.m
@@ -3,7 +3,7 @@ if (app.player1turn == 1)
     app.numCardsDealt = app.numCardsDealt + 1;
     app.Label.Text = num2str(str2double(app.Label.Text) + str2double(app.shuffledCards{app.numCardsDealt,2}));
     %updates label value for Player 1 score
-    score1 = str2num(app.Label.Text);
+    score1 = str2double(app.Label.Text);
     if score1<21
         app.player1turn = 1;
     elseif score1==21
