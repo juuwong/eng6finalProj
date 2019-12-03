dbe13191bdf57f8295055e891864db8e6b3db74a
Justin Wong <juuwong@ucdavis.edu>
2019-12-02 21:44:28 -0800

    Hit functionality complete for Player 1 and 2. next is Dealer play

--- hit.m
+++ hit.m
@@ -1,6 +1,37 @@
 function hit(app) 
+if (app.player1turn == 1)
+    app.numCardsDealt = app.numCardsDealt + 1;
 
-app.numCardsDealt = app.numCardsDealt + 1;
-app.Label.Text = num2str(str2double(app.Label.Text) + str2double(app.shuffledCards{app.numCardsDealt,2}));%updates score
+    app.Label.Text = num2str(str2double(app.Label.Text) + str2double(app.shuffledCards{app.numCardsDealt,2}));
+    %updates label value for Player 1 score
+    score1 = str2num(app.Label.Text);
+    if score1<21
+        app.player1turn = 1;
+    elseif score1==21
+        app.player1turn=0;
+        %fprintf("Blackjack!")
+    elseif score1>21
+        app.player1turn = 0;
+    else
+        app.ErrorLabel.Visible = 'on';
+    end
+end        
+if (app.player2turn == 1)
+    app.numCardsDealt = app.numCardsDealt + 1;
+    app.Label_2.Text = num2str(str2double(app.Label_2.Text) + str2double(app.shuffledCards{app.numCardsDealt,2}));
+    %updates player 2 score
+    score2 = str2num(app.Label_2.Text);
+    if score2<21
+        app.player2turn = 1;
+    elseif score2==21
+        app.player2turn=0;
+        %fprintf("Blackjack!")
+    elseif score2>21
+        app.player2turn = 0;
+    else
+        app.ErrorLabel.Visible = 'on';
+    end
+end
+
 
 end
--- test.m
+++ test.m
@@ -1,11 +1,14 @@
-imagefiles = dir('cardPhotos/*.png');      
-nfiles = length(imagefiles); 
-for ii=1:nfiles
-   currentfilename = imagefiles(ii).name;
-   app.imgFileNames{ii} = char(currentfilename);
+
+numCardsDealt = 0;
+numCardsDealt = app.numCardsDealt + 1;
+Label.Text = num2str(str2double("9") + str2double("4"))%updates score
+
+score = str2num(Label.Text)
+
+if score<21
+    fprintf("Keep going! Your score is %d",score)    
+elseif score>21
+    fprintf("you lose!")
+else
+    fprintf("invalid score!")
 end
-    
-cardId = [1:52];
-v = [10 2 3 4 5 6 7 8 9 1 10 10 10];
-cardValue = repelem(v,4);
-app.cards = [cardId', cardValue', "cardPhotos/" + string(app.imgFileNames)'];
