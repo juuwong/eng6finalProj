083496ddedbac0686bdb6c0265acb9c13449042d
May Eusebio <mariellejde@gmail.com>
2019-12-05 10:31:49 -0800

    Working updateBalanceTest when plyr1 stands and dealer busts

--- updateBalanceTest.m
+++ updateBalanceTest.m
@@ -1,4 +1,4 @@
 function updateBalanceTest(app)
-if(str2double(app.Label_3) > 21)
-app.Balance1EditField.Value = 69;
+if(str2double(app.Label_3.Text) > 21)
+app.Balance1EditField.Value = app.Balance1EditField.Value + (2 * app.Bet1EditField.Value);
 end
