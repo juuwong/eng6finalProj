58f406dc4cb2c9559be3e8ddc4b1c780f8d24915
May Eusebio <mariellejde@gmail.com>
2019-12-05 10:44:18 -0800

    Working test with player 1

--- updateBalanceTest.m
+++ updateBalanceTest.m
@@ -1,4 +1,13 @@
 function updateBalanceTest(app)
-if(str2double(app.Label_3.Text) > 21)
-app.Balance1EditField.Value = app.Balance1EditField.Value + (2 * app.Bet1EditField.Value);
+if(app.numPlayers == 1)
+    % if dealer busts & plyr1 under 21
+    if(str2double(app.Label_3.Text) > 21 && str2double(app.Label.Text) <= 21) 
+        app.Balance1EditField.Value = app.Balance1EditField.Value + (2 * app.Bet1EditField.Value);
+    % dealer under 21 and player under 21    
+    elseif(str2double(app.Label_3.Text) <= 21 && str2double(app.Label.Text) <= 21)
+        if(str2double(app.Label.Text) > str2double(app.Label_3.Text))
+            app.Balance1EditField.Value = app.Balance1EditField.Value + (2 * app.Bet1EditField.Value);
+        end
+    end
+end
 end
